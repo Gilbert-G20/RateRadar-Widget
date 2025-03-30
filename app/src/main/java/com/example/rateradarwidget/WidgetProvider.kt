5e5f621f9c53f6ec430d2ebda8a623ddf2a270fd
Gilbert Gomez <gomeznarvaezgilbert@gmail.com>
2025-03-30 19:18:39 -0400

    class to handle widget updates v1.2

--- app/src/main/java/com/example/rateradarwidget/WidgetProvider.kt
+++ app/src/main/java/com/example/rateradarwidget/WidgetProvider.kt
@@ -1,11 +1,9 @@
 package com.example.rateradarwidget
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
 import android.widget.RemoteViews
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
